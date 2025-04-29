323a22d32263d551b868fc3d4c955a9cbee664f6
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-04-29 08:30:01 +0000

    launch file for filter

--- cpp/avg_imu.cpp
+++ cpp/avg_imu.cpp
@@ -1,24 +1,43 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 
+sensor_msgs::Imu imu_msg;
+float alpha;
+
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
-    // Process the IMU data here
-    ROS_INFO("IMU Data: Orientation: [%f, %f, %f, %f], Angular Velocity: [%f, %f, %f], Linear Acceleration: [%f, %f, %f]",
-             msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w,
-             msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z,
-             msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+    imu_msg.angular_velocity.x = (1-alpha) * imu_msg.angular_velocity.x + (alpha) * msg->angular_velocity.x;
+    imu_msg.angular_velocity.y = (1-alpha) * imu_msg.angular_velocity.y + (alpha) * msg->angular_velocity.y;
+    imu_msg.angular_velocity.z = (1-alpha) * imu_msg.angular_velocity.z + (alpha) * msg->angular_velocity.z;
+    imu_msg.linear_acceleration.x = (1-alpha) * imu_msg.linear_acceleration.x + (alpha) * msg->linear_acceleration.x;
+    imu_msg.linear_acceleration.y = (1-alpha) * imu_msg.linear_acceleration.y + (alpha) * msg->linear_acceleration.y;
+    imu_msg.linear_acceleration.z = (1-alpha) * imu_msg.linear_acceleration.z + (alpha) * msg->linear_acceleration.z;
+
 }
 
 int main(int argc, char** argv)
-{
+{    
+    imu_msg.angular_velocity.x = 0;
+    imu_msg.angular_velocity.y = 0;
+    imu_msg.angular_velocity.z = 0;
+    imu_msg.linear_acceleration.x = 0;
+    imu_msg.linear_acceleration.y = 0;
+    imu_msg.linear_acceleration.z = 0;
+    imu_msg.orientation.x = 0;
+
     // Initialize ROS
     ros::init(argc, argv, "avg_imu");
 
     // Create a node handle
     ros::NodeHandle nh;
 
-    ros::Subscriber camera_info_sub = nh.subscribe("/camera/depth/camera_info", 100, imuCallback);
+    std::string imu_topic;
+    nh.getParam("imu_topic", imu_topic);
+    nh.getParam("alpha", alpha);
+
+    std::cout<<imu_topic<<std::endl;
+
+    ros::Subscriber camera_info_sub = nh.subscribe(imu_topic, 100, imuCallback);
 
     // Spin to keep the node alive
     ros::spin();
